a02f3893bb6843cdf203c6dc9ce47888303286d4
manelly67 <118464447+manelly67@users.noreply.github.com>
2024-07-05 15:56:52 -0500

    Add files via upload

---
+++ modules_classes/pretty-print-tree.js
@@ -0,0 +1,16 @@
+import {Node} from './balanced-bst.js';
+
+const prettyPrint = (node=new Node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+export {prettyPrint};
